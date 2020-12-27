a2aceecaf44ae3a16a7cbc6ed1480c3f40710961
Ilya-And <ilya.and2011@yandex.ru>
2020-12-28 00:53:49 +0300

    Добавьте файлы проекта.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("BuildLintels")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BuildLintels")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("2d549130-413a-4947-9ad6-439544614295")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StartClass.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.Attributes;
+namespace BuildLintels
+
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    [RegenerationAttribute(RegenerationOption.Manual)]
+    public class StartClass : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            FilteredElementCollector newFilterDoors = new FilteredElementCollector(doc);
+            FilteredElementCollector newFilterWindows = new FilteredElementCollector(doc);
+
+            //Create collections
+
+            ICollection<Element> allDoors = newFilterDoors.OfCategory(BuiltInCategory.OST_Doors).WhereElementIsNotElementType().ToElements();
+            ICollection<Element> allWindows = newFilterWindows.OfCategory(BuiltInCategory.OST_Windows).WhereElementIsNotElementType().ToElements();
+
+
+            // Create enpty dictionary
+            var dictOpensByWall = new Dictionary<string, Dictionary<string, List<FamilyInstance>>>();
+
+            // Iterate over the elements of each category and fill in the dictionary
+
+            FillDictionary(allDoors, dictOpensByWall);
+            FillDictionary(allWindows, dictOpensByWall);
+
+
+            UserWind userWind = new UserWind(dictOpensByWall);
+            userWind.Show();
+            return Result.Succeeded;
+
+
+
+        }
+
+        //Method to iterate over the elements of each category and fill in the dictionary
+        private void FillDictionary(ICollection<Element> collectionElements, Dictionary<string, Dictionary<string, List<FamilyInstance>>> dict)
+        {
+            foreach (Element element in collectionElements)
+            {
+
+                FamilyInstance el = element as FamilyInstance;
+                Wall wall = (Wall)el.Host;
+                WallType sWall = wall.WallType;
+                string modelGroup = sWall.get_Parameter(BuiltInParameter.ALL_MODEL_MODEL).AsString();
+                string cat = el.Category.Name;
+                double width = el.get_Parameter(BuiltInParameter.FAMILY_WIDTH_PARAM).AsDouble();
+
+
+
+                if (!dict.ContainsKey(modelGroup))
+                {
+                    var newDict = new Dictionary<string, List<FamilyInstance>>();
+                    var newList = new List<FamilyInstance>();
+                    newList.Add(el);
+                    newDict[cat] = newList;
+
+                    dict.Add(modelGroup, newDict);
+                }
+                else
+                {
+
+                    if (!dict[modelGroup].ContainsKey(cat))
+                    {
+                        var newListInstance = new List<FamilyInstance>();
+                        newListInstance.Add(el);
+                        dict[modelGroup][cat] = newListInstance;
+                    }
+                    else
+                    {
+                        dict[modelGroup][cat].Add(el);
+                    }
+
+
+                }
+
+            }
+
+        }
+
+    }
+
+
+}
